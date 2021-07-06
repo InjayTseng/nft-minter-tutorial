77569094bad577f6bfcfe68479f9995a98a2dcd4
David Tseng <inin2001@hotmail.com>
2021-07-06 22:32:36 +0800

    + first commit

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -1,33 +1,78 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+  mintNFT,
+  estimateGas,
+} from './utils/interact'
 
 const Minter = (props) => {
-
   //State variables
-  const [walletAddress, setWallet] = useState("");
-  const [status, setStatus] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [url, setURL] = useState("");
- 
-  useEffect(async () => { //TODO: implement
-    
-  }, []);
+  const [walletAddress, setWallet] = useState('')
+  const [status, setStatus] = useState('')
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [recipient, setRecipient] = useState('')
+  const [url, setURL] = useState('')
 
-  const connectWalletPressed = async () => { //TODO: implement
-   
-  };
+  useEffect(async () => {
+    //TODO: implement
+    const { address, status } = await getCurrentWalletConnected()
+    setWallet(address)
+    setStatus(status)
+    addWalletListener()
+  }, [])
 
-  const onMintPressed = async () => { //TODO: implement
-    
-  };
+  const connectWalletPressed = async () => {
+    //TODO: implement
+    const walletResponse = await connectWallet()
+    setStatus(walletResponse.status)
+    setWallet(walletResponse.address)
+  }
+
+  const onMintPressed = async () => {
+    //TODO: implement
+    const { status } = await mintNFT(url, name, description, recipient)
+    setStatus(status)
+  }
+
+  const onEstimateGas = async () => {
+    //TODO: implement
+    await estimateGas(url, name, description)
+  }
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0])
+          setStatus('👆🏽 Write a message in the text-field above.')
+        } else {
+          setWallet('')
+          setStatus('🦊 Connect to Metamask using the top right button.')
+        }
+      })
+    } else {
+      setStatus(
+        <p>
+          {' '}
+          🦊{' '}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>,
+      )
+    }
+  }
 
   return (
     <div className="Minter">
       <button id="walletButton" onClick={connectWalletPressed}>
         {walletAddress.length > 0 ? (
-          "Connected: " +
+          'Connected: ' +
           String(walletAddress).substring(0, 6) +
-          "..." +
+          '...' +
           String(walletAddress).substring(38)
         ) : (
           <span>Connect Wallet</span>
@@ -58,15 +103,22 @@ const Minter = (props) => {
           placeholder="e.g. Even cooler than cryptokitties ;)"
           onChange={(event) => setDescription(event.target.value)}
         />
+        <h2>🍺 Recipient: </h2>
+        <input
+          type="text"
+          placeholder="e.g. 0x26Ea0b1d8a0258265Ca5e838cb8a161E5d709031 ;)"
+          onChange={(event) => setRecipient(event.target.value)}
+        />
       </form>
       <button id="mintButton" onClick={onMintPressed}>
         Mint NFT
       </button>
-      <p id="status">
-        {status}
-      </p>
+      <button id="mintButton" onClick={onEstimateGas}>
+        Estimate Gas
+      </button>
+      <p id="status">{status}</p>
     </div>
-  );
-};
+  )
+}
 
-export default Minter;
+export default Minter
