1a9a63b30f27da1bb4dfa8d9af8447705a078043
David Tseng <inin2001@hotmail.com>
2021-08-04 02:27:20 +0800

    + route not working

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
           <Link to="/users">Users </Link> */}
           <Navbar bg="light" expand="lg">
             <Container>
-              <Navbar.Brand href="/">DXT</Navbar.Brand>
+              <Navbar.Brand to="/">DXT</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="/minter">Minter</Nav.Link>
-                  <Nav.Link href="/about">About</Nav.Link>
+                  <Nav.Link to="/minter">Minter</Nav.Link>
+                  <Nav.Link to="/about">About</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
             </Container>
@@ -30,8 +30,8 @@ function App() {
             <Route path="/about">
               <About />
             </Route>
-            <Route path="/users">
-              <Users />
+            <Route path="/">
+              <Minter />
             </Route>
             <Route path="/minter">
               <Minter />
